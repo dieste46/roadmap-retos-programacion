c7dfac375225fecf4e78b00caca2d615e5d31af4
Jorge Dieste <dieste46@hotmail.com>
2024-01-09 22:12:09 +0100

    #00 - Phyton

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/dieste46.py
@@ -0,0 +1,24 @@
+# Esto es un comentario de una línea en Python
+# URL oficial de Python: https://www.python.org
+
+"""
+Esto es un comentario
+de varias líneas
+en Python
+"""
+
+# Creación de una variable
+mi_variable = "Hola, mundo!"
+
+# Python no tiene constantes en el sentido estricto, pero por convención, las variables en mayúsculas se tratan como constantes
+MI_CONSTANTE = "Soy una constante"
+
+# Variables representando todos los tipos de datos primitivos en Python
+cadena = "Esto es una cadena de texto"
+entero = 10
+booleano = True
+flotante = 20.5
+complejo = 1j
+
+# Imprimir por terminal
+print("¡Hola, Python!")
